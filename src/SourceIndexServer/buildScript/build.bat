2f6d94823fa36d0a8794b72b5413d29aac21eb6f
ZhangJin <nbnbnb.zhang@gmail.com>
2018-12-10 22:48:23 +0800

    Remove implicit-package-references

--- src/SourceIndexServer/buildScript/build.bat
+++ src/SourceIndexServer/buildScript/build.bat
@@ -2,5 +2,5 @@
 set outDir=D:\GitRepos\SourceBrowser\src\SourceIndexServer\Index
 rmdir /s/q %outDir%
 D:\GitRepos\SourceBrowser\src\HtmlGenerator\bin\Release\net461\HtmlGenerator.exe /out:%outDir% /in:projects.txt
-echo 生成完成
+echo Completed!
 pause
